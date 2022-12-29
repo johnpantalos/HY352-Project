39634ece372c4e71ba61545bf01e67cb3f8eee19
anestiskivro <anestiskivro@gmail.com>
2022-12-29 21:54:24 +0200

    aaaa

--- json_lib.h
+++ json_lib.h
@@ -5,22 +5,33 @@
 #include <map>
 #ifndef json_lib_h
 #define json_lib_h
+
 struct Json_Type{
 
     private:
     std::vector<float> array;
     std::vector<Json_Type> dataobj; // or map?
-
-    std::string name;
-    std::string value;
-
+    int number;
+    bool expression;
+    std::string name_expr;
     public:
-    
-    void setName(std::string Name){
-        this->name=Name;
+    Json_Type(std::string stringExpr){
+
     }
-    void setValue(std::string Value){
-        this->value=Value;
+    Json_Type(bool expression){
+
+    }
+    Json_Type(int number){
+
+    }
+    Json_Type(std::vector<Json_Type> Object){
+
+    }
+    Json_Type(std::vector<float> array){
+
+    }
+    void setName(std::string Name){
+        this->name_expr=Name;
     }
     void setArray(std::vector<float> Table){
         this->array=Table;
@@ -29,10 +40,7 @@ struct Json_Type{
         this->dataobj=Data;
     }
     std::string getName(){
-        return this->name;
-    }
-    std::string getValue(){
-        return this->value;
+        return this->name_expr;
     }
     std::vector<float> getArray(){
         return this->array;
@@ -43,7 +51,7 @@ struct Json_Type{
 
     bool is_Empty(){
         if(this->getArray().size()==0 && this->getDataObj().size()==0
-            && this->getName()=="" && this->getValue()==""){
+            && this->getName()==""){
                 return true;
         }else{
             return false;
