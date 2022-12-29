b3bff2cb78c623485ea1c704cdbe2fb97ad8b4ba
anestiskivro <anestiskivro@gmail.com>
2022-12-29 19:02:40 +0200

    aaaa

--- json_lib.h
+++ json_lib.h
@@ -8,11 +8,36 @@ struct Json_Type{
 
     private:
     std::vector<float> array;
-    std::vector<Json_Type> data; // or map?
+    std::vector<Json_Type> dataobj; // or map?
 
     std::string name;
     std::string str;
 
     public:
+
+    void setName(std::string Name){
+        this->name=Name;
+    }
+    void setString(std::string String){
+        this->str=String;
+    }
+    void setArray(std::vector<float> Table){
+        this->array=Table;
+    }
+    void setDataObject(std::vector<Json_Type> Data){
+        this->dataobj=Data;
+    }
+    std::string getName(){
+        return this->name;
+    }
+    std::string getString(){
+        return this->str;
+    }
+    std::vector<float> getArray(){
+        return this->array;
+    }
+    std::vector<Json_Type> getDataObj(){
+        return this->dataobj;
+    }
     
 };
