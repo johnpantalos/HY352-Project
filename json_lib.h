d79e9a87c50ca5e5842606fde2d550cbd4b433b1
anestiskivro <anestiskivro@gmail.com>
2022-12-29 22:29:49 +0200

    aaaa

--- json_lib.h
+++ json_lib.h
@@ -8,56 +8,7 @@
 
 struct Json_Type{
 
-    private:
    
-    // or map?
-    int number;
-    bool expression;
-    std::string name_expr;
-    public:
-    Json_Type(std::string stringExpr){
-
-    }
-    Json_Type(bool expression){
-
-    }
-    Json_Type(int number):number(number){
-
-    }
-    Json_Type(std::vector<Json_Type> Object){
-
-    }
-    Json_Type(std::vector<float> array){
-
-    }
-    void setName(std::string Name){
-        this->name_expr=Name;
-    }
-    void setArray(std::vector<float> Table){
-        this->array=Table;
-    }
-    void setDataObject(std::vector<Json_Type> Data){
-        this->dataobj=Data;
-    }
-    std::string getName(){
-        return this->name_expr;
-    }
-    std::vector<float> getArray(){
-        return this->array;
-    }
-    std::vector<Json_Type> getDataObj(){
-        return this->dataobj;
-    }
-
-    bool is_Empty(){
-        if(this->getArray().size()==0 && this->getDataObj().size()==0
-            && this->getName()==""){
-                return true;
-        }else{
-            return false;
-        }
-    }
-
 };
 
 class Values
