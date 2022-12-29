db25af02fda0cd9fd5fe0ae01ab6764751b12ac1
anestiskivro <anestiskivro@gmail.com>
2022-12-29 22:54:43 +0200

    aaaa

--- json_lib.h
+++ json_lib.h
@@ -5,12 +5,17 @@
 #include <map>
 #ifndef json_lib_h
 #define json_lib_h
+#define JSON(var) Json_Type json
+#define STRING(name) new String
+#define NUMBER(name) new Number
+#define OBJECT(name) new Object
+#define ARRAY(name) new Array
 
 struct Json_Type{
     public:
         std::string Name;
 
-        public Json_Type(std::string name) {
+        public: Json_Type(std::string name) {
             this->Name = name;
         }
 };
@@ -39,11 +44,11 @@ class Number : Values
         int numberInt = 0;
         float numberFloat = 0.0;
 
-    public Number(int intNum) {
+    public: Number(int intNum) {
         this->numberInt = intNum;
     }
 
-    public Number(float floatNum) {
+    public: Number(float floatNum) {
         this->numberFloat = floatNum;
     }
 };
@@ -51,15 +56,19 @@ class Number : Values
 class Object : Values
 {
      std::vector<Json_Type> dataobj;
+     public: Object(std::vector<Json_Type> Object){
+        this->dataobj=Object;
+     }
 
 }; 
 
 class Array : Values
 {
      std::vector<float> array;
+     public: Array( std::vector<float> Array){
+        this->array=Array;
+     }
 };
-
-
 
 
 #endif
