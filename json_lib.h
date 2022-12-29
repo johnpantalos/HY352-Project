2e17e067b188c0a1fb6645d7177c45474f4db041
anestiskivro <anestiskivro@gmail.com>
2022-12-29 18:42:56 +0200

    aaaa

--- json_lib.h
+++ json_lib.h
@@ -1,3 +1,18 @@
-struct Json_mytype{
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
 
+struct Json_Type{
+
+    private:
+    std::vector<float> array;
+    std::vector<Json_Type> data; // or map?
+
+    std::string name;
+    std::string str;
+
+    public:
+    
 };
