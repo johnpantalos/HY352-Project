91800f9b5c9e4ab134b02939a53c1a5f663ac0e5
csd4348 <97064037+csd4348@users.noreply.github.com>
2022-12-31 01:24:35 +0200

    Update json_file.cpp

--- json_file.cpp
+++ json_file.cpp
@@ -1,17 +1,21 @@
 #include "json_lib.h"
 
- //define emptyObj json with empty OBJECT
-JSON(emptyObj) = OBJECT{}
+//JSON(emptyObj) = OBJECT{}
+////eimaste koukloi      
+
+PROGRAM_BEGIN
+
+//define emptyObj json with empty OBJECT
+JSON(emptyObj) = NUMBER(1022.0)
+JSON(emptyObj) = STRING("1022")
+JSON(emptyObj) = TRUE
+
 //define emptyArray json with empty array
-JSON(emptyArray) = ARRAY
-
-//eimai oraios
-JSON(emptyArray) = ARRAY
+JSON(emptyArray) = ARRAY[]
 
 
+JSON(week_temperatures) = ARRAY[NUMBER(20)]
+JSON(week_temperatures) = ARRAY[ NUMBER(20), NUMBER(19.5), NUMBER(19), NUMBER(20), NUMBER(19), NUMBER(18.5), NUMBER(19) ]
+Json_Type week_temperatures = new Json_Type(std::vector<Json_Type> array)
 
-
-
-
-
-lallalallalllllalla
+PROGRAM_END
