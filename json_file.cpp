10ec8c2d6c115d6c1258942e7318ef02140876ce
csd4348 <97064037+csd4348@users.noreply.github.com>
2023-01-03 01:35:29 +0200

    Update json_file.cpp

--- json_file.cpp
+++ json_file.cpp
@@ -6,16 +6,25 @@
 PROGRAM_BEGIN
 
 //define emptyObj json with empty OBJECT
-JSON(emptyObj) = NUMBER(1022.0)
-JSON(emptyObj) = STRING("1022")
-JSON(emptyObj) = TRUE
+JSON(empObj1) = NUMBER(1022.0)
+JSON(empObj2) = STRING("1022")
+JSON(empObj3) = TRUE
 
+//JSON(obj)=OBJECT{}
 //define emptyArray json with empty array
-JSON(emptyArray) = ARRAY[]
+//JSON(emptyArray) = ARRAY
+
+JSON(week_temperatures2) = ARRAY[NUMBER(20), NUMBER(19.5), NUMBER(19), NUMBER(20), NUMBER(19), NUMBER(18.5), NUMBER(19)]
 
 
-JSON(week_temperatures) = ARRAY[NUMBER(20)]
-JSON(week_temperatures) = ARRAY[ NUMBER(20), NUMBER(19.5), NUMBER(19), NUMBER(20), NUMBER(19), NUMBER(18.5), NUMBER(19) ]
-Json_Type week_temperatures = new Json_Type(std::vector<Json_Type> array)
+//#define ARRAY new Json_Type(std::vector<Json_Type*>Array)
+//#define ARRAY new Json_Type(/*std::vector<Json_Type*>Array*/)
+
+
+
+//JSON(week_temperatures) = ARRAY[NUMBER(20)]
+
+							// vector1.push_back(new Json_Type(19.5))
+//Json_Type week_temperatures = new Json_Type(std::vector<Json_Type> array)
 
 PROGRAM_END
