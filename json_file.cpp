418c6d7ecb0f594c78a6d2fff3cf050ff03cc58c
csd4348 <97064037+csd4348@users.noreply.github.com>
2023-01-05 01:33:26 +0200

    Update json_file.cpp

--- json_file.cpp
+++ json_file.cpp
@@ -1,21 +1,24 @@
 #include "json_lib.h"
 
 //JSON(emptyObj) = OBJECT{}
-////eimaste koukloi      
+   
 
 PROGRAM_BEGIN
 
 //define emptyObj json with empty OBJECT
-JSON(empObj1) = NUMBER(1022.0)
+
+JSON(empObj1) = NUMBER(1022.5)
 JSON(empObj2) = STRING("1022")
 JSON(empObj3) = TRUE
 
-//JSON(obj)=OBJECT{}
+
+// JSON(obj)=OBJECT{}
 //define emptyArray json with empty array
-//JSON(emptyArray) = ARRAY
+JSON(emptyArray) = ARRAY
 
-JSON(week_temperatures2) = ARRAY[NUMBER(20), NUMBER(19.5), NUMBER(19), NUMBER(20), NUMBER(19), NUMBER(18.5), NUMBER(19)]
+JSON(week_temperatures2) = ARRAY[ NUMBER(20), NUMBER(19), NUMBER(18), NUMBER(17), NUMBER(16), NUMBER(15), NUMBER(14) ]
 
+//JSON(object)=OBJECT
 
 //#define ARRAY new Json_Type(std::vector<Json_Type*>Array)
 //#define ARRAY new Json_Type(/*std::vector<Json_Type*>Array*/)
