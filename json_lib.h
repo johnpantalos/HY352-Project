a38f11255c318e5126ad405d3a572e46667ff494
anestiskivro <121625979+anestiskivro@users.noreply.github.com>
2023-01-10 19:05:00 +0200

    Update json_lib.h

--- json_lib.h
+++ json_lib.h
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <iostream>
-#include <fstream>
 #include <list>
 #include <string>
 #include <vector>
@@ -18,18 +16,19 @@
 #define STRING(name) Json_Type(name)
 #define NUMBER(num) Json_Type(num)
 #define ARRAY Json_Type(TMParray)
-#define OBJECT Json_Type(TMPobj)
+#define OBJECT *new Json_Type
 #define TRUE Json_Type(true)
 #define FALSE Json_Type(false)
-#define NULL Json_Type(nullptr)
-#define KEY(name) Json_Type name; AddToVector(#name, name); name = (1>2) ? Json_Type()
+#define NULL __null
+// #define KEY(name) Json_Type name; AddToVector(#name, name); name = (1>2) ? Json_Type()
+#define KEY(name) Json_Type(tmp = #name) = 0 ? 0
 #define PRINT ;std::cout <<
 #define PROGRAM_BEGIN                            \
     int main()                                   \
     {                                            \
         std::string tmp;                         \
-        std::map<std::string, Json_Type> TMPobj; \
-        std::vector<Json_Type> TMParray;
+        std::initializer_list<Json_Type>TMPobj; \
+        std::vector<Json_Type> TMParray; \
         
 #define SIZEOF(var) switch (var.GetFlag()) {                                        \
 case 4:                                                                             \
@@ -66,8 +65,9 @@ int Helper = 0;
 struct Json_Type
 {
 public:
-    std::string CurrName;
-    Json_Type *CurrJson = nullptr;
+ std::string CurrName = "None";
+ Json_Type *CurrJson = nullptr;
+
 private:
     int flag = 10;
     std::string Name = "";
@@ -80,20 +80,21 @@ private:
     std::vector<Json_Type> array;
 
 public:
-    Json_Type() { CurrJson = this; this->flag = 0; std::cout << "called 0\n"; }
+    Json_Type() { CurrJson = this; this->flag = 0; std::cout << "called 0\n"; };
 
-    Json_Type(std::string str) : string(str) { CurrJson = this;/*std::cout << "called 1\n";*/ this->flag = 1; }
+    Json_Type( std::string str) : string(str) { CurrJson = this;/*std::cout << "called 1\n";*/ this->flag = 1; };
 
-    Json_Type(int intNum) : numberInt(intNum) { CurrJson = this;/*std::cout << "called 2\n";*/ this->flag = 2; }
+    Json_Type( int intNum) : numberInt(intNum) { CurrJson = this;/*std::cout << "called 2\n";*/ this->flag = 2; };
 
-    Json_Type(double DoubleNum) : numberDouble(DoubleNum) { CurrJson = this;/*std::cout << "called 3\n";*/ this->flag = 3; }
+    Json_Type( double DoubleNum) : numberDouble(DoubleNum) { CurrJson = this;/*std::cout << "called 3\n";*/ this->flag = 3; };
 
-    Json_Type(std::list<Json_Type> Object)
+    Json_Type(std::initializer_list <Json_Type> Object)
     {
-    std::list<Json_Type>::iterator it = this->dataobj.begin();
-    dataobj.insert(it,Object);
-    /*std::cout << "called 4\n";*/ this->flag = 4; }
-    Json_Type(std::vector<Json_Type> Array) {std::cout << "called 5\n"; this->flag = 5; }
+    std::list<Json_Type>::iterator it;
+    it=this->dataobj.begin();
+    it=this->dataobj.insert(it,Object);
+    /*std::cout << "called 4\n";*/ this->flag = 4; };
+    Json_Type(std::vector<Json_Type> Array) {std::cout << "called 5\n"; this->flag = 5; };
 
     void setName(std::string name) {
         this->Name = name;
@@ -164,54 +165,54 @@ public:
     //JSON(LALA) = STRING("P")
     //JSON(LALA1) = NUMBER(10)
     //JSON(LALA2) = ARRAY[NUMBER(9), NUMBER(8)]
-    //Json_Type operator=(Json_Type obj)
-    //{
-    //    //std::cout << this->GetName();                             TYPWNEI TO ONOMAA!!
-    //    //obj.setName(this->GetName());
+    Json_Type operator=(Json_Type obj)
+    {
+       //std::cout << this->GetName();                             TYPWNEI TO ONOMAA!!
+       //obj.setName(this->GetName());
 
-    //    //CurrName = this->GetName();
-    //    switch (obj.GetFlag()) {
-    //        case 1:
-    //            this->setString(obj.string);
-    //            AddToVector(this->GetName(), *this);                                    //                      'H obj          ????????????????
-    //            //std::cout << "1 added to vector\n";
-    //            break;
-    //        case 2:
-    //            this->setInt(obj.numberInt);
-    //            if (obj.numberInt == 1) this->setBool(true);
-    //            else if (obj.numberInt == 0) this->setBool(false);
-    //            AddToVector(this->GetName(), *this);                                    //                      'H obj          ????????????????
-    //            //std::cout << "2 added to vector\n";
-    //            break;
-    //        case 3:
-    //            this->setdouble(obj.numberDouble);
-    //            AddToVector(this->GetName(), *this);                                    //                      'H obj          ????????????????
-    //            //std::cout << "3 added to vector\n";
-    //            break;
-    //        case 4:     // OBJECT
-    //            this->setObject(obj.dataobj);
-    //            AddToVector(this->GetName(), *this);                                    //                      'H obj          ????????????????
-    //            //std::cout << "4 added to vector\n";
-    //            break;
-    //        case 5:     // ARRAY
-    //            //this->setArray(obj.array);
-    //            for (int j = 0; j < CollectionVariables.begin()->second.GetArray().size(); j++)
-    //            {
-    //                std::cout << CollectionVariables.begin()->second.GetArray().at(j).GetNumberInt() << "\n";
-    //            }
-    //            std::cout << obj.GetName();
-    //            //AddToVector(this->GetName(), *this);                                    //                      'H obj          ????????????????
-    //            std::cout << "5 added to vector\n";
-    //            break;
-    //        default:
-    //            AddToVector(this->GetName(), *this);                                    //                      'H obj          ????????????????
-    //            break;
-    //    }
-    //    return obj;
-    //}
+       //CurrName = this->GetName();
+       switch (obj.GetFlag()) {
+           case 1:
+               this->setString(obj.string);
+               AddToVector(this->GetName(), *this);                                    //                      'H obj          ????????????????
+               //std::cout << "1 added to vector\n";
+               break;
+           case 2:
+               this->setInt(obj.numberInt);
+               if (obj.numberInt == 1) this->setBool(true);
+               else if (obj.numberInt == 0) this->setBool(false);
+               AddToVector(this->GetName(), *this);                                    //                      'H obj          ????????????????
+               //std::cout << "2 added to vector\n";
+               break;
+           case 3:
+               this->setdouble(obj.numberDouble);
+               AddToVector(this->GetName(), *this);                                    //                      'H obj          ????????????????
+               //std::cout << "3 added to vector\n";
+               break;
+           case 4:     // OBJECT
+               this->setObject(obj.dataobj);
+               AddToVector(this->GetName(), *this);                                    //                      'H obj          ????????????????
+               //std::cout << "4 added to vector\n";
+               break;
+           case 5:     // ARRAY
+               //this->setArray(obj.array);
+            //    for (int j = 0; j < CollectionVariables.begin()->second.GetArray().size(); j++)
+            //    {
+            //        std::cout << CollectionVariables.begin()->second.GetArray().at(j).GetNumberInt() << "\n";
+            //    }
+               std::cout << obj.GetName();
+               //AddToVector(this->GetName(), *this);                                    //                      'H obj          ????????????????
+               std::cout << "5 added to vector\n";
+               break;
+           default:
+               AddToVector(this->GetName(), *this);                                    //                      'H obj          ????????????????
+               break;
+       }
+       return obj;
+    }
     Json_Type operator[](Json_Type obj);
 
-    Json_Type& operator,(Json_Type obj1);
+    Json_Type operator,(Json_Type obj1);
     bool IS_EMPTY();
     std::string TYPE_OF();
 };
@@ -220,6 +221,7 @@ std::unordered_map<std::string, Json_Type> CollectionVariables;
 
 Json_Type Json_Type::operator[](Json_Type obj)
 {
+    std::cout << "\n[]\n";
     Helper = 0;
     this->setArray(obj.array);
     
@@ -227,30 +229,40 @@ Json_Type Json_Type::operator[](Json_Type obj)
     //obj.setName(CurrName);
     //AddToVector(this->GetName(), *this);
     //std::cout << this->GetName();
-    std::cout << "\n[]\n";
-    auto it = CollectionVariables.begin();
-    /*for (int j = 0; j < it->second.array.size(); j++)
-    {
-        std::cout << it->second.array.at(j).GetNumberInt() << "\n";
-    }*/
-    //std::cout<<obj.GetNumberInt()<< ";a;a\n";
-    //AddToVector(this->GetName(),*this);                                                       //  AddToVector(CurrName, it->second);
-    it->second = *this;
+    // auto it = CollectionVariables.begin();
+    // for (int j = 0; j < it->second.array.size(); j++)
+    // {
+    //     std::cout << it->second.array.at(j).GetNumberInt() << "\n";
+    // }
+    for(int i=0;i<obj.GetArray().size();i++){
+        std::cout<<obj.GetNumberInt()<<"\n";
+    }
+     std::cout<<this->GetNumberInt()<< ";a;a\n";
+    std::cout<<obj.GetNumberInt()<< ";a;a\n";
+    AddToVector(this->GetName(),*this);
+                                                           //  AddToVector(CurrName, it->second);
+    // it->second = *this;
     return *this;
 };
 
 
 //JSON(week_temperatures2) = ARRAY[NUMBER(20), NUMBER(19), NUMBER(18), NUMBER(17), NUMBER(16), NUMBER(15), NUMBER(14)]
 
-Json_Type& Json_Type::operator,(Json_Type obj1)
+Json_Type Json_Type::operator,(Json_Type obj1)
 {
     std::cout << "\n,,,\n";
-    auto it = CollectionVariables.begin();
     //std::cout << it->first;
-
+    Json_Type temp;
+    temp.array=obj1.GetArray();
+    temp.numberDouble=obj1.GetNumberDouble();
+    temp.numberInt=obj1.GetNumberInt();
+    temp.string=obj1.GetString();
+    temp.boolean=obj1.GetBool();
+    temp.dataobj=obj1.GetDataobj();
+    temp.flag=obj1.GetFlag();
+    temp.Name=obj1.GetName();
     if (Helper == 0) {
-        it->second.array.push_back(*this);
-        // this->array.push_back(*this);
+        this->array.push_back(*this);
         //std::cout << this->GetNumberInt() << " ";
         Helper++;
     }
@@ -262,18 +274,16 @@ Json_Type& Json_Type::operator,(Json_Type obj1)
     //    std::cout << this->array.at(j).GetNumberInt() << "\n";
     //}
     
-    it->second.array.push_back(obj1);
-    AddToVector(this->GetName(),*this);
     //                                                                          it->second.array.push_back(temp);
     //std::cout << obj1.GetNumberInt() << " ";
+//    for (int j = 0; j < it->second.array.size(); j++)
+//     {
+//        std::cout << it->second.array.at(j).GetNumberInt() << "\n";
+//     }
+    // std::cout << it->first << "\n";
+    // std::cout<<obj1.GetName()<<"okok"; 
     
-    //for (int j = 0; j < it->second.array.size(); j++)
-    //{
-    //    std::cout << it->second.array.at(j).GetNumberInt() << "\n";
-    //std::cout << it->first << "\n
-    //std::cout<<obj1.GetName()<<"okok";
-    
-    return *this;
+    return obj1;
 }
 
 inline void Json_Type::AddToVector(std::string(NameVar), Json_Type variable)
@@ -328,9 +338,6 @@ bool Json_Type::IS_EMPTY(){
         }
 
     }
-    
-//std::string Json_Type::CurrName = "None";
-//Json_Type Json_Type::CurrJson;
 
 
 //void AddToVector(std::string(NameVar), Json_Type variable)
@@ -364,3 +371,4 @@ bool Json_Type::IS_EMPTY(){
 // }
 
 #endif
+
