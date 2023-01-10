873fcb2e537b8e436bf44aa297b2b2c627984c81
csd4348 <97064037+csd4348@users.noreply.github.com>
2023-01-11 01:15:35 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,30 +5,34 @@ PROGRAM_BEGIN
 
 //define emptyObj json with empty OBJECT
 
-//JSON(empObj1) = NUMBER(1022)
-//JSON(papa) = STRING("lala")
-// JSON(emp2) = STRING("lala111")
-
-
-
-//JSON(obj) = OBJECT{}
-//define emptyArray json with empty array
-//JSON(emptyArray) = ARRAY
-
 // JSON(empObj3) = TRUE
-JSON(week_temperatures2) = ARRAY[NUMBER(20), STRING("lala111"), NUMBER(18), NUMBER(17), NUMBER(16), NUMBER(15), NUMBER(14)]
-
+JSON(week_temperatures2) = ARRAY[NUMBER(20), NUMBER(19), NUMBER(18), NUMBER(17), NUMBER(16), NUMBER(15), NUMBER(14)]
+JSON(empObj1) = NUMBER(1022)
+JSON(papa) = STRING("lala")
+JSON(emp2) = STRING("lala111")
 //JSON(object) = OBJECT;
 
 //KEY(title) : STRING("Gone Girl");
 
 //KEY(papapa) : STRING("frapaa")
 
-JSON(book) = OBJECT{
-	KEY(published) : NUMBER(2012),
-	KEY(type): STRING("Thriller")
-}
+//JSON(book) = OBJECT{
+//	KEY(published) : NUMBER(2012),
+//	KEY(type) : STRING("Thriller")
+//}
 
+PRINT empObj1
+PRINT papa
+PRINT week_temperatures2
+
+SIZE_OF(week_temperatures2)
+
+SIZE_OF(papa)
+
+TYPE_OF(papa)
+
+IS_EMPTY(week_temperatures2)
+IS_EMPTY(papa)
 //JSON(week_temperatures) = ARRAY[NUMBER(20)]
 
 							// vector1.push_back(new Json_Type(19.5))
