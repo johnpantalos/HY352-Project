4f06864fc054e0101395c9b817245f523959b14e
csd4348 <97064037+csd4348@users.noreply.github.com>
2023-01-11 01:16:03 +0200

    Update json_lib.h

--- json_lib.h
+++ json_lib.h
@@ -20,34 +20,27 @@
 #define TRUE Json_Type(true)
 #define FALSE Json_Type(false)
 #define NULL __null
-// #define KEY(name) Json_Type name; AddToVector(#name, name); name = (1>2) ? Json_Type()
-#define KEY(name) Json_Type(tmp = #name) = 0 ? 0
-#define PRINT ;std::cout <<
+#define PRINT ;std::cout << "\n" <<
+#define SIZE_OF(value) ;std::cout << "\n" << value.SIZEOF(value)
+#define TYPE_OF(value) ;std::cout << "\n" << "The type of " << \
+value.GetName() << " is : " << value.TYPEOF(value) << "\n"
+#define IS_EMPTY(value) ;std::cout << "\n" << value.ISEMPTY(value)
+
+#define KEY(name) Json_Type name = (1>2) ? Json_Type()
+// #define KEY(name) Json_Type(tmp = #name) = 0 ? 0
+
 #define PROGRAM_BEGIN                            \
     int main()                                   \
     {                                            \
         std::string tmp;                         \
-        std::initializer_list<Json_Type>TMPobj; \
-        std::vector<Json_Type> TMParray; \
-        
-#define SIZEOF(var) switch (var.GetFlag()) {                                        \
-case 4:                                                                             \
-    std::cout << "The size of object is : " << var.GetArray().size() << "\n";       \
-case 5:                                                                             \
-    std::cout << "The size of array is : " << var.GetDataObj().size() << "\n";      \
-    break;                                                                          \
-default:                                                                            \
-    std::cout << " The size of variable is : 1\n";                                  \
-    break;                                                                          \
-}                                                                                       
+        std::initializer_list<Json_Type>TMPobj;  \
+        std::vector<Json_Type> TMParray; 
 
 #define PROGRAM_END         \
     ;return 0;              \
     }
 //#define KEY(name) Json_Type(tmp = #name)
 //#define KEY(name) TMPobj.insert(std::pair<<std::string, Json_Type>(#name, mytmp) :               // sto telos tou object    (to diko mou map) = TMPobj; TMPobj = clear()
-//#define KEY(name) Json_Type *name = new Json_Type:               // sto telos tou object    (to diko mou map) = TMPobj; TMPobj = clear()
-//x = (y < 10) ? 30 : 40;
 
 //JSON(book) = OBJECT{
 //
@@ -57,21 +50,14 @@ default:                                                                        
 //    }
 //}
 
-
-// std::string CurrName;
-// Json_Type CurrJson;
-
 struct Json_Type
 {
-public:
- std::string CurrName = "None";
- Json_Type *CurrJson = nullptr;
 
 private:
     int flag = 10;
     std::string Name = "";
     bool boolean = false; // True, False
-    int *ptr = nullptr;   // null
+    int* ptr = nullptr;   // null
     std::string string = "";
     int numberInt = 77777;
     double numberDouble = 77777.0;
@@ -79,21 +65,22 @@ private:
     std::vector<Json_Type> array;
 
 public:
-    Json_Type() { CurrJson = this; this->flag = 0; std::cout << "called 0\n"; };
+    Json_Type() { this->flag = 0; };
 
-    Json_Type( std::string str) : string(str) { CurrJson = this;/*std::cout << "called 1\n";*/ this->flag = 1; };
+    Json_Type(std::string str) : string(str) { this->flag = 1; };
 
-    Json_Type( int intNum) : numberInt(intNum) { CurrJson = this;/*std::cout << "called 2\n";*/ this->flag = 2; };
+    Json_Type(int intNum) : numberInt(intNum) { this->flag = 2; };
 
-    Json_Type( double DoubleNum) : numberDouble(DoubleNum) { CurrJson = this;/*std::cout << "called 3\n";*/ this->flag = 3; };
+    Json_Type(double DoubleNum) : numberDouble(DoubleNum) { this->flag = 3; };
 
     Json_Type(std::initializer_list <Json_Type> Object)
     {
-    std::list<Json_Type>::iterator it;
-    it=this->dataobj.begin();
-    it=this->dataobj.insert(it,Object);
-    /*std::cout << "called 4\n";*/ this->flag = 4; };
-    Json_Type(std::vector<Json_Type> Array) {std::cout << "called 5\n"; this->flag = 5; };
+        std::list<Json_Type>::iterator it;
+        it = this->dataobj.begin();
+        it = this->dataobj.insert(it, Object);
+        this->flag = 4;
+    };
+    Json_Type(std::vector<Json_Type> Array) { this->flag = 5; };
 
     void setName(std::string name) {
         this->Name = name;
@@ -110,7 +97,7 @@ public:
     void setdouble(double DoubleNum) {
         this->numberDouble = DoubleNum;
     }
-    
+
     void setBool(bool Boolean) {
         this->boolean = Boolean;
     }
@@ -120,7 +107,7 @@ public:
     }
 
     void setArray(std::vector<Json_Type> Array) {
-        this->array=Array;
+        this->array = Array;
     }
 
     void setFlag(int Flag) {
@@ -146,7 +133,7 @@ public:
     double GetNumberDouble() {
         return this->numberDouble;
     }
-    
+
     double GetBool() {
         return this->boolean;
     }
@@ -159,63 +146,48 @@ public:
         return this->array;
     }
 
-    void AddToVector(std::string(NameVar), Json_Type variable);
+    //void AddToVector(std::string(NameVar), Json_Type variable);
 
-    //JSON(LALA) = STRING("P")
-    //JSON(LALA1) = NUMBER(10)
-    //JSON(LALA2) = ARRAY[NUMBER(9), NUMBER(8)]
     Json_Type operator=(Json_Type obj)
     {
-       //std::cout << this->GetName();                             TYPWNEI TO ONOMAA!!
-       //obj.setName(this->GetName());
-
-       //CurrName = this->GetName();
-       switch (obj.GetFlag()) {
-           case 1:
-               this->setString(obj.string);
-               AddToVector(this->GetName(), *this);                                    //                      'H obj          ????????????????
-               //std::cout << "1 added to vector\n";
-               break;
-           case 2:
-               this->setInt(obj.numberInt);
-               if (obj.numberInt == 1) this->setBool(true);
-               else if (obj.numberInt == 0) this->setBool(false);
-               AddToVector(this->GetName(), *this);                                    //                      'H obj          ????????????????
-               //std::cout << "2 added to vector\n";
-               break;
-           case 3:
-               this->setdouble(obj.numberDouble);
-               AddToVector(this->GetName(), *this);                                    //                      'H obj          ????????????????
-               //std::cout << "3 added to vector\n";
-               break;
-           case 4:     // OBJECT
-               this->setObject(obj.dataobj);
-               AddToVector(this->GetName(), *this);                                    //                      'H obj          ????????????????
-               //std::cout << "4 added to vector\n";
-               break;
-           case 5:     // ARRAY
-               //this->setArray(obj.array);
-            //    for (int j = 0; j < CollectionVariables.begin()->second.GetArray().size(); j++)
-            //    {
-            //        std::cout << CollectionVariables.begin()->second.GetArray().at(j).GetNumberInt() << "\n";
-            //    }
-               std::cout << obj.GetName();
-               //AddToVector(this->GetName(), *this);                                    //                      'H obj          ????????????????
-               std::cout << "5 added to vector\n";
-               break;
-           default:
-               AddToVector(this->GetName(), *this);                                    //                      'H obj          ????????????????
-               break;
-       }
-       return obj;
+        //std::cout << this->GetName()<<"\n";                             //TYPWNEI TO ONOMAA!!
+        
+        this->setFlag(obj.GetFlag());
+        switch (obj.GetFlag()) {
+        case 1:
+            this->setString(obj.string);
+            break;
+        case 2:
+            this->setInt(obj.numberInt);
+            if (obj.numberInt == 1) this->setBool(true);
+            else if (obj.numberInt == 0) this->setBool(false);
+            break;
+        case 3:
+            this->setdouble(obj.numberDouble);
+            break;
+        case 4:     // OBJECT
+            this->dataobj.push_back(obj); 
+            break;
+        case 5:     // ARRAY
+             for(int i = 0; i < obj.GetArray().size() ; i++){
+                this->array.push_back(obj.GetArray()[i]);
+             }
+            std::cout << obj.GetName();
+            break;
+        default:
+            break;
+        }
+        return *this;
     }
     Json_Type operator[](Json_Type obj);
 
     Json_Type operator,(Json_Type obj1);
-    bool IS_EMPTY();
-    std::string TYPE_OF();
+
+    friend std::ostream &operator<<(std::ostream &os, Json_Type &probj);
+    std::string SIZEOF(Json_Type& probj);
+    std::string ISEMPTY(Json_Type& probj);
+    std::string TYPEOF(Json_Type& probj);
 };
 
 #endif
-
 
