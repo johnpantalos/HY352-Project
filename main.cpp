f2a89090b5e9fb841e1394c18bb9b61f05af6c41
csd4348 <97064037+csd4348@users.noreply.github.com>
2023-05-24 23:54:20 +0300

    Update main.cpp
    Final commit, don't work the array of objects

--- main.cpp
+++ main.cpp
@@ -1,42 +1,88 @@
 #include "json_lib.h"
 PROGRAM_BEGIN
 
-//define emptyObj json with empty OBJECT
+JSON(EMPTYARRAY) = ARRAY
 
-JSON(empObj3) = TRUE
-JSON(week_temperatures2) = ARRAY[NUMBER(20), NUMBER(19), NUMBER(18), NUMBER(17), NUMBER(16), NUMBER(15), NUMBER(14)]
-JSON(empObj1) = NUMBER(1022)
-JSON(papa) = STRING("lala")
-JSON(emp2) = STRING("lala111")
-//JSON(object) = OBJECT;
+PRINT EMPTYARRAY
 
+JSON(strofara) = STRING("WRE MIA STROFARA")
+JSON(anest) = STRING(" ELA ANESTH")
+JSON(str_anest) = STRING("WRE MIA STROFARA") + STRING(" ELA ANESTH")
+JSON(noumer) = NUMBER(1022)
+JSON(True) = TRUE
+JSON(week_temperatures2) = ARRAY[NUMBER(20), NUMBER(19), NUMBER(18)]
+JSON(week_temperatures2_next) = ARRAY[NUMBER(17), NUMBER(16), NUMBER(15), NUMBER(14)]
+JSON(week) = ARRAY[NUMBER(20), NUMBER(19), NUMBER(18)] + ARRAY[NUMBER(17), NUMBER(16), NUMBER(15), NUMBER(14)]
 
-SET week_temperatures2[2] ASSIGN NUMBER(23)
+PRINT strofara
+PRINT anest
+
+PRINT str_anest
 
 PRINT week_temperatures2
-
-//JSON(book) = OBJECT{
-//    KEY(title) : STRING("Gone Girl"),
-//    KEY(published) : NUMBER(2012),
-//    KEY(published1) : NUMBER(2012.2),
-//    KEY(type) : STRING("Thriller"),
-//    KEY(grades) : ARRAY[NUMBER(10), NUMBER(7), NUMBER(5), NUMBER(6), NUMBER(8), NUMBER(9), NUMBER(7)],
-//    KEY(object):OBJECT{
-//        KEY(name) : STRING("Antetokounmpo"),
-//        KEY(number) : NUMBER(34),
-//        KEY(fever) : NUMBER(37),
-//        KEY(championship) : STRING("2021"),
-//        KEY(points) : ARRAY[NUMBER(20), NUMBER(23), NUMBER(40), NUMBER(34), NUMBER(19), NUMBER(25), NUMBER(28)],
-//        KEY(object1) : OBJECT{KEY(name2) : STRING("Antetokounmpo11")}
-//    }
-//}
-//PRINT empObj1
-//SIZE_OF(week_temperatures2)
-//PRINT papa
-//PRINT week_temperatures2
-//SIZE_OF(book)
+PRINT week_temperatures2_next
+PRINT week
 
 
+JSON(ADD) = NUMBER(1022) + NUMBER(1023)
+JSON(SUB) = NUMBER(1022) - NUMBER(1023)
+JSON(MULTI) = NUMBER(1) * NUMBER(1023)
+JSON(DIV) = NUMBER(4) / NUMBER(2)
+JSON(MOD) = NUMBER(5) % NUMBER(2)
+JSON(SYGKRISI) = NUMBER(1022) > NUMBER(1023)
+JSON(SYGKRISI1) = NUMBER(1022) >= NUMBER(1022)
+JSON(SYGKRISI2) = NUMBER(1022) < NUMBER(1022)
+JSON(SYGKRISI3) = NUMBER(1022) <= NUMBER(1023)
+
+PRINT ADD
+PRINT SUB
+PRINT MULTI
+PRINT DIV
+PRINT MOD
+PRINT SYGKRISI
+PRINT SYGKRISI1
+PRINT SYGKRISI2
+PRINT SYGKRISI3
+
+JSON(LUCIFER) = STRING("Hello ") + STRING("There!")
+PRINT LUCIFER
+
+JSON(book) = OBJECT{
+    KEY(title) : STRING("Gone Girl"),
+    KEY(published) : NUMBER(2012),
+    KEY(published1) : NUMBER(2012.2),
+    KEY(type) : STRING("Thriller"),
+    KEY(grades) : ARRAY[NUMBER(10), NUMBER(7), NUMBER(5), NUMBER(6), NUMBER(8), NUMBER(9), NUMBER(7)],
+    KEY(object) : OBJECT{
+        KEY(name) : STRING("Antetokounmpo"),
+        KEY(number) : NUMBER(34),
+        KEY(fever) : NUMBER(37),
+        KEY(championship) : STRING("2021"),
+        KEY(points) : ARRAY[NUMBER(20), NUMBER(23), NUMBER(40), NUMBER(34), NUMBER(19), NUMBER(25), NUMBER(28)],
+    }
+}
+
+JSON(book1) = OBJECT{
+    KEY(title) : STRING("John") + STRING(" Panta"),
+    KEY(published) : NUMBER(2023),
+    KEY(type) : STRING("Comedy") + STRING(" Club") 
+}
+
+JSON(book2) = OBJECT{
+    KEY(title) : STRING("Gone Girl"),
+    KEY(published4) : NUMBER(2012),
+    KEY(type) : STRING("Thriller") 
+}
+
+PRINT book
+PRINT book1
+PRINT book2
+
+JSON(book_book1) = book + book1
+PRINT book_book1
+
+JSON(boo1_book2) = book1 + book2
+PRINT boo1_book2
 
 
 //JSON(students) = ARRAY[
@@ -49,21 +95,60 @@ PRINT week_temperatures2
 //
 //    ] }]
     
+//PRINT students
 
-// PRINT book
+PRINT HAS_KEY(book, "published1")
+PRINT HAS_KEY(book, "published")
+PRINT HAS_KEY(book, "type")
+PRINT HAS_KEY(book, "type1")
+PRINT HAS_KEY(book, "title")
+PRINT HAS_KEY(boo1_book2, "published4")
+PRINT HAS_KEY(book, "grades1")
 
-//PRINT book["title"]
-//PRINT HAS_KEY(book, "published1")
-//PRINT HAS_KEY(book, "published")
-//PRINT HAS_KEY(book, "type")
-//PRINT HAS_KEY(book, "type1")
-//PRINT HAS_KEY(book, "title")
-//PRINT HAS_KEY(book, "grades")
-//PRINT HAS_KEY(book, "grades1")
+SIZE_OF(book1)
+SIZE_OF(week)
+IS_EMPTY(week)
+IS_EMPTY(EMPTYARRAY)
+TYPE_OF(strofara)
+TYPE_OF(ADD)
+TYPE_OF(book)
 
-//JSON(week_temperatures) = ARRAY[NUMBER(20)]
+PRINT book["title"]
+PRINT book1["published"]
+PRINT book2["title"]
+PRINT book["grades"]
 
-							// vector1.push_back(new Json_Type(19.5))
-//Json_Type week_temperatures = new Json_Type(std::vector<Json_Type> array)
+JSON(CHECK1) = TRUE && TRUE
+JSON(CHECK2) = FALSE && TRUE
+JSON(CHECK3) = TRUE && FALSE
+JSON(CHECK4) = FALSE && FALSE
+
+JSON(CHECK5) = TRUE || TRUE
+JSON(CHECK6) = FALSE || TRUE
+JSON(CHECK7) = TRUE || FALSE
+JSON(CHECK8) = FALSE || FALSE
+
+JSON(CHECK9) = FALSE
+JSON(CHECK10) = !FALSE
+
+PRINT(CHECK1)
+PRINT(CHECK2)
+PRINT(CHECK3)
+PRINT(CHECK4)
+PRINT(CHECK5)
+PRINT(CHECK6)
+PRINT(CHECK7)
+PRINT(CHECK8)
+PRINT(CHECK9)
+PRINT(CHECK10)
+
+JSON(EQUAL) = (STRING("Pa") == STRING("Pa"))
+JSON(EQUAL1) = NUMBER(1022) == NUMBER(1023)
+JSON(UNEQUAL) = NUMBER(1022) == STRING("Pa")
+JSON(UNEQUAL1) = TRUE == STRING("Pa")
+
+PRINT EQUAL1
+PRINT UNEQUAL
+PRINT UNEQUAL1
 
 PROGRAM_END
